9205ad7effdbd0f64a3dba2828a029387981422e
leonardommascarenhas <leonardommascarenhas@gmail.com>
2023-02-02 21:59:50 -0300

    commit

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 require('dotenv').config()
-
 const express = require('express');
 const app = express();
 
@@ -17,6 +16,24 @@ const path = require('path');
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.resolve(__dirname, 'public')));
+
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const flash = require('connect-flash');
+
+const sessionOptions = session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7, //tempo q vai durar o cookie (7dias)
+    httpOnly: true
+  },
+  store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING })
+});
+
+app.use(sessionOptions);
+app.use(flash());
 
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,6 +1,7 @@
 const HomeModel = require('../models/HomeModel')
 
 exports.paginaInicial = (req, res) => {
+  console.log(req.session.usuario)
   res.render('index');
 };
 
